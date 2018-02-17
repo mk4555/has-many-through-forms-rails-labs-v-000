f974703ac1149bb4e47cba6f938fe75af2676a55
Min Kim <mk4555@nyu.edu>
2018-02-17 03:39:04 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   def users_attributes=(user_attributes)
     category_attributes.values.each do |user_attribute|
-      category = Category.find_or_create_by(category_attribute)
+      category = Category.find_or_create_by(user_attribute)
       self.categories << category
     end
   end
