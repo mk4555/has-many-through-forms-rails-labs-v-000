ef8eb9cc0c2920a7972645fbca3a07f78e5467aa
Min Kim <mk4555@nyu.edu>
2018-02-17 03:39:09 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many :posts, through: :comments
 
   def users_attributes=(user_attributes)
-    category_attributes.values.each do |user_attribute|
-      category = Category.find_or_create_by(user_attribute)
+    users_attributes.values.each do |user_attribute|
+      category = User.find_or_create_by(user_attribute)
       self.categories << category
     end
   end
